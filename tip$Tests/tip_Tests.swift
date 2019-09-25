3f5ca02a92e12f7229e563f95564e197923d735f
Jiale Qiu <jiale98chinoguay@gmail.com>
2019-09-25 10:24:48 -0400

    initial commit

---
+++ tip$Tests/tip_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tip_Tests.swift
+//  tip$Tests
+//
+//  Created by Jiale Qiu on 9/24/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import XCTest
+@testable import tip_
+
+class tip_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
